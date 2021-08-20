db1caaf4a2344902fb43373b9e3a64291a0e8acf
Ryan Pereira <44448885+RyanPereiraS@users.noreply.github.com>
2021-08-20 20:58:18 -0300

    Add files via upload

--- WEB/index.php
+++ WEB/index.php
@@ -1,14 +1,89 @@
-<?php 
-    session_start();
-    if(isset($_SESSION['logado'])){
-        echo $_SESSION['usuario'];
-    }
-?>
-<a href="login/">LOGIn</a>
-<form action="" method="post">
-    <input type="submit" value="Log-out">
-</form>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-<?php
-    session_destroy();
-?>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="css home/estilizacao.css">
+	<title>Home WAAP</title>
+</head>
+<body>
+
+	<div class="clearfix">
+		<div id="void">	
+	</div>
+
+	<div id="Logo">
+		<img src="img/logoparalela.png" title="logo WAAP">
+	</div>
+
+	<nav id="menu-home">
+		<ul>
+			<li><a href="#">Adote</a></li>
+			<li><a href="#">Doe</a></li>
+			<li><a href="#">Emergência</a></li>
+			<li><a href="#">Notícias</a></li>
+			<li><a href="login/cadastrar.php" >Cadastrar</a></li>
+			<li><a href="login/" >Login</a></li>
+		</ul>
+	</nav>
+
+	<div class="linha">
+		<hr>
+	</div>
+
+	<div id="imgilustrativa">
+		<img src="assets/imgTeste.jpg" title="imagem ilustrativa">
+	</div>
+
+	<div id="textone">
+		Texto totalmente aleatoria ate pq eu nao sei oq vem aqui tlgd ent eu vou apenas escrever alguma coisa sem sentido so pra mim poder podelar no css e arrumar de uma forma bunitinha tlgd ate por que e é um trabalho totalmente importante sabe como é ne vou usar na minha intrevista de emprego porque eu sou lindooo jkkkkkk bricadeiras a parte e que acabou minhas ideias do que escrever, alias se esta querendo uma serie para assistir, procure Suits na Netflix... AdeusTexto totalmente aleatoria ate pq eu nao sei oq vem aqui tlgd ent eu vou apenas escrever alguma coisa sem sentido so pra mim poder podelar no css e arrumar de uma forma bunitinha tlgd ate por que e é um trabalho totalmente importante sabe como é ne vou usar na minha intrevista de emprego porque eu sou lindooo jkkkkkk bricadeiras a parte e que acabou minhas ideias do que escrever, alias se esta querendo uma serie para assistir, procure Suits na Netflix... Adeus
+	</div>
+
+	<div id="circulo">
+		<img src="assets/coracaoajudar.jpg" title="Coracao WAAP"/>
+	</div>
+
+	<div id="ajude">
+		<h2>
+			Ajude a Ajudar !!
+		</h2>
+	</div>
+
+	<div id="ajudebot">
+		<a href="#">Ajude o Projeto</a>
+	</div>
+
+	<div id="ajudebottwo">
+		<a href="#">Doe Para ONG'S</a>
+	</div>
+
+	<div class="linha">
+		<hr>
+	</div>
+
+	<p>Dicas de Doação</p>
+
+	<div id="slider">
+		<img class="selected" src="img noticias/WAAP.png" alt="Image1">
+		<img src="img noticias/roxo.png" alt="Image2">
+		<img src="img noticias/Logo3.png" alt="Image3">
+		<img src="img noticias/Logo1.png" alt="Image4">
+	</div>
+
+	<script src="JS/script.js"></script>
+
+	<div id="sliderdois">
+		<img src="img noticias/cachorro1.jpg" alt="image1">
+	</div>
+
+	<div id="textDoacao">
+		Doe ração, acessórios e remédios veterinários
+		<br>
+		<br>
+		Ajude os animalzinhos que precisam de ração, acessórios ou até mesmo remédios veterinários,
+		você pode estar ajudando doando para ONG'S, ou se preferir ajude os animais de rua do seu
+		bairro, juntos podemos diminuir o nivel de animais sem um lar !!!
+	</div>
+
+</body>
+</html>
