35f7405f7e4714ff4ec3a8a93bda73f84c7e2cc7
Paulo1Ddan <81446114+Paulo1Ddan@users.noreply.github.com>
2021-09-08 17:48:35 -0300

    Delete WEB directory

--- WEB/painel/ong/painel.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    if(isset($_SESSION['logado'])){
-        if($_SESSION['logado']){
-            header("Location: ../");
-        }
-    }
-?>
--- WEB/utils/functions.php
+++
@@ -1,6 +0,0 @@
-<?php 
-    function str_contains(string $haystack, string $needle): bool
-    {
-        return '' === $needle || false !== strpos($haystack, $needle);
-    }
-?>
